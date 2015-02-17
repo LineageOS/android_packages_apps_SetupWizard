77c129f8b940532298d9c3cb8ae72d051ad3ed74
cretin45 <cretin45@gmail.com>
2015-02-17 11:00:08 -0800

    SetupWizard: Fix regression setting UI flags with onscreen keys
    Change-Id: I9fbbf4368ebe8faabbe4d66b5cacbf9ee2f3be36

--- src/com/cyanogenmod/setupwizard/ui/SetupWizardActivity.java
+++ src/com/cyanogenmod/setupwizard/ui/SetupWizardActivity.java
@@ -62,8 +62,6 @@ public class SetupWizardActivity extends Activity implements SetupDataCallbacks 
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         setContentView(R.layout.setup_main);
         getWindow().setWindowAnimations(android.R.anim.fade_in);
         mRootView = findViewById(R.id.root);
@@ -128,6 +126,10 @@ public class SetupWizardActivity extends Activity implements SetupDataCallbacks 
 
     @Override
     protected void onResume() {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         super.onResume();
         onPageTreeChanged();
         enableButtonBar(true);
