d57f616611c83b4e4a5cad16f04e2451ef97ed48
cretin45 <cretin45@gmail.com>
2015-08-21 11:01:51 -0700

    SetupWizard: Bypass gms setup if unable to hit G servers.
    If connected to an interface that can't connect to internet, gms account setup can get into a state without and navigation buttons. This fix attempts to ping the gms captive portal check page prior to launching account setup. If it can't hit that page, move along.
    
    PS 5 - Account for some FRP locked scenarios.
    
    Addresses SAMBAR-411
    
    Change-Id: I79fe29f9f78e7a534740a179bee91872b2dc1e19

--- src/com/cyanogenmod/setupwizard/setup/GmsAccountPage.java
+++ src/com/cyanogenmod/setupwizard/setup/GmsAccountPage.java
@@ -16,6 +16,7 @@
 
 package com.cyanogenmod.setupwizard.setup;
 
+import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.accounts.AccountManagerCallback;
 import android.accounts.AccountManagerFuture;
@@ -29,8 +30,10 @@ import android.content.ContentQueryMap;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
 import android.service.persistentdata.PersistentDataBlockManager;
 import android.util.Log;
@@ -42,9 +45,34 @@ import com.cyanogenmod.setupwizard.ui.LoadingFragment;
 import com.cyanogenmod.setupwizard.util.SetupWizardUtils;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Observable;
 import java.util.Observer;
 
+
+/**
+ * This page controls the launching into GSM account setup and optionally gms restore.
+ *
+ * Several factors influence the behavior of this page.
+ *
+ * - FRP (Factory Reset Protection) prevents a user from advancing pass this page without
+ *   authenticating with an account that was previously set up on this device. Once a user
+ *   authenticates, they are prompted to create an account. At this point, they can also skip
+ *   setting up a GMS account. If a user does setup a GMS account FRP is enabled with that
+ *   account as soon as it is added. If a user restarts the device prior to OOBE completion,
+ *   the OOBE will see that FRP is enabled when it is restarted. To get around launching back
+ *   into account setup, the account name that was setup prior to restart is saved until OOBE
+ *   completion. If OOBE sees that mSignedInAccountName is populated, it will advance. Possibly to
+ *   restore if that hasn't completed.
+ *
+ * - If FRP isn't enabled and OOBE cannot ping Google, it will auto advance. This can happen if
+ *   a network interface is up but cannot reach the interwebs. GMS account setup has a bug where
+ *   the error page won't show and previous/next buttons if this happens, trapping the user. If this
+ *   condition is reached and FRP is enabled, there is no choice but to launch into GMS setup.
+ *
+ */
 public class GmsAccountPage extends SetupPage {
 
     public static final String TAG = "GmsAccountPage";
@@ -53,16 +81,38 @@ public class GmsAccountPage extends SetupPage {
     private static final String RESTORE_WIZARD_SCRIPT =
             "android.resource://com.google.android.setupwizard/xml/wizard_script";
 
+    private static final String KEY_GMS_ACCOUNT = ".account";
+    private static final String KEY_RESTORE_COMPLETED = ".restore";
+
+    private static final String GMS_SERVER = "clients3.google.com";
+    private static final int GMS_SOCKET_TIMEOUT_MS = 10000;
+    private URL mGmsUri;
+
     private ContentQueryMap mContentQueryMap;
     private Observer mSettingsObserver;
+    private SharedPreferences mSharedPreferences;
 
     private boolean mBackupEnabled = false;
+    private boolean mSignedIn = false;
+    private boolean mRestoreComplete = false;
 
-    private Fragment mFragment;
+    // If a user signs in to GMS successfully, but restarts the device before Setup complete,
+    // the device gets FRP locked. This stores the account name until Setup is complete.
+    private String mSignedInAccountName;
+
+    private final Handler mHandler = new Handler();
+
+    private LoadingFragment mFragment;
 
     public GmsAccountPage(final Context context, SetupDataCallbacks callbacks) {
         super(context, callbacks);
         final ContentResolver res = context.getContentResolver();
+        mSharedPreferences = context.getSharedPreferences(TAG, Context.MODE_PRIVATE);
+        mSignedInAccountName = mSharedPreferences.getString(KEY_GMS_ACCOUNT, null);
+        mRestoreComplete = mSharedPreferences.getBoolean(KEY_RESTORE_COMPLETED, false);
+        if (mSignedInAccountName != null) {
+            mSignedIn = true;
+        }
         mBackupEnabled = Settings.Secure.getInt(res,
                 Settings.Secure.BACKUP_ENABLED, 0) == 1;
         mSettingsObserver = new Observer() {
@@ -79,11 +129,16 @@ public class GmsAccountPage extends SetupPage {
                 null);
         mContentQueryMap = new ContentQueryMap(settingsCursor, Settings.System.NAME, true, null);
         mContentQueryMap.addObserver(mSettingsObserver);
+        try {
+            mGmsUri = new URL("http://" + GMS_SERVER + "/generate_204");
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "Not a valid url" + e);
+        }
     }
 
     @Override
     public Fragment getFragment(FragmentManager fragmentManager, int action) {
-        mFragment = fragmentManager.findFragmentByTag(getKey());
+        mFragment = (LoadingFragment)fragmentManager.findFragmentByTag(getKey());
         if (mFragment == null) {
             Bundle args = new Bundle();
             args.putString(Page.KEY_PAGE_ARGUMENT, getKey());
@@ -115,30 +170,23 @@ public class GmsAccountPage extends SetupPage {
             getCallbacks().onPreviousPage();
         } else {
             super.doLoadAction(fragmentManager, action);
-            launchGmsAccountSetup();
+            checkForGmsConnection();
         }
     }
 
     @Override
     public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == SetupWizardApp.REQUEST_CODE_SETUP_GMS) {
-            if (!mBackupEnabled && SetupWizardUtils.isOwner() && resultCode == Activity.RESULT_OK) {
-                SetupStats.addEvent(SetupStats.Categories.EXTERNAL_PAGE_LOAD,
-                        SetupStats.Action.EXTERNAL_PAGE_RESULT,
-                        SetupStats.Label.GMS_ACCOUNT, "success");
-                launchGmsRestorePage();
-            } else {
-                handleResult(requestCode, resultCode);
-            }
+            handleAccountSetupResult(resultCode);
         } else if (requestCode == SetupWizardApp.REQUEST_CODE_RESTORE_GMS) {
-            handleResult(requestCode, resultCode);
-            setHidden(true);
+            handleRestoreResult(resultCode);
         }
         return true;
     }
 
     @Override
     public void onFinishSetup() {
+        mSharedPreferences.edit().clear();
         try {
             if (mContentQueryMap != null) {
                 mContentQueryMap.close();
@@ -148,32 +196,67 @@ public class GmsAccountPage extends SetupPage {
         }
     }
 
-    private void handleResult(int requestCode, int resultCode) {
+    private void handleAccountSetupResult(int resultCode) {
+        if (SetupWizardUtils.isOwner() &&
+                resultCode == Activity.RESULT_OK) {
+            final Account[] accounts
+                    = AccountManager.get(mContext)
+                    .getAccountsByType(SetupWizardApp.ACCOUNT_TYPE_GMS);
+            mSignedIn = accounts.length > 0;
+            if (mSignedIn) {
+                // Persist the account name until completion. That way, if a user restarts the
+                // device, OOBE can resume.
+                mSharedPreferences.edit().putString(KEY_GMS_ACCOUNT, accounts[0].name).commit();
+            } else {
+                mSharedPreferences.edit().clear();
+            }
+        }
+        if (!mBackupEnabled &&
+                SetupWizardUtils.isOwner() &&
+                mSignedIn &&
+                resultCode == Activity.RESULT_OK) {
+            SetupStats.addEvent(SetupStats.Categories.EXTERNAL_PAGE_LOAD,
+                    SetupStats.Action.EXTERNAL_PAGE_RESULT,
+                    SetupStats.Label.GMS_ACCOUNT, "success");
+            launchGmsRestorePage();
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            SetupStats.addEvent(SetupStats.Categories.EXTERNAL_PAGE_LOAD,
+                    SetupStats.Action.EXTERNAL_PAGE_RESULT,
+                    SetupStats.Label.GMS_ACCOUNT, "canceled");
+            getCallbacks().onPreviousPage();
+        } else {
+            if (canSkip()) {
+                SetupStats.addEvent(SetupStats.Categories.EXTERNAL_PAGE_LOAD,
+                        SetupStats.Action.EXTERNAL_PAGE_RESULT,
+                        SetupStats.Label.GMS_ACCOUNT, "skipped");
+                getCallbacks().onNextPage();
+            } else {
+                getCallbacks().onPreviousPage();
+            }
+        }
+    }
+
+    private void handleRestoreResult(int resultCode) {
         if (resultCode == Activity.RESULT_CANCELED) {
             SetupStats.addEvent(SetupStats.Categories.EXTERNAL_PAGE_LOAD,
                     SetupStats.Action.EXTERNAL_PAGE_RESULT,
-                    requestCode == SetupWizardApp.REQUEST_CODE_SETUP_GMS ?
-                            SetupStats.Label.GMS_ACCOUNT : SetupStats.Label.RESTORE, "canceled");
+                    SetupStats.Label.RESTORE, "canceled");
             getCallbacks().onPreviousPage();
         }  else {
             if (resultCode == Activity.RESULT_OK) {
                 SetupStats.addEvent(SetupStats.Categories.EXTERNAL_PAGE_LOAD,
                         SetupStats.Action.EXTERNAL_PAGE_RESULT,
-                        requestCode == SetupWizardApp.REQUEST_CODE_SETUP_GMS ?
-                                SetupStats.Label.GMS_ACCOUNT : SetupStats.Label.RESTORE, "success");
+                        SetupStats.Label.RESTORE, "success");
                 getCallbacks().onNextPage();
             } else {
-                if (canSkip()) {
-                    SetupStats.addEvent(SetupStats.Categories.EXTERNAL_PAGE_LOAD,
-                            SetupStats.Action.EXTERNAL_PAGE_RESULT,
-                            requestCode == SetupWizardApp.REQUEST_CODE_SETUP_GMS ?
-                                    SetupStats.Label.GMS_ACCOUNT : SetupStats.Label.RESTORE, "skipped");
-                    getCallbacks().onNextPage();
-                } else {
-                    getCallbacks().onPreviousPage();
-                }
+                SetupStats.addEvent(SetupStats.Categories.EXTERNAL_PAGE_LOAD,
+                        SetupStats.Action.EXTERNAL_PAGE_RESULT,
+                        SetupStats.Label.RESTORE, "skipped");
+                getCallbacks().onNextPage();
+
             }
-            if (SetupWizardUtils.accountExists(mContext, SetupWizardApp.ACCOUNT_TYPE_GMS)) {
+            if (mSignedIn) {
+                mSharedPreferences.edit().putBoolean(KEY_RESTORE_COMPLETED, true).commit();
                 setHidden(true);
             }
         }
@@ -201,7 +284,6 @@ public class GmsAccountPage extends SetupPage {
                 mFragment.startActivityForResult(
                         intent,
                         SetupWizardApp.REQUEST_CODE_RESTORE_GMS, options.toBundle());
-                return;
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -214,9 +296,62 @@ public class GmsAccountPage extends SetupPage {
     public boolean canSkip() {
         final PersistentDataBlockManager pdbManager = (PersistentDataBlockManager)
                 mContext.getSystemService(Context.PERSISTENT_DATA_BLOCK_SERVICE);
-        return pdbManager == null
+        return mSignedIn
+                || pdbManager == null
                 || pdbManager.getDataBlockSize() == 0
                 || pdbManager.getOemUnlockEnabled();
+    }
+
+    private void checkForGmsConnection() {
+        if (SetupWizardUtils.accountExists(mContext, SetupWizardApp.ACCOUNT_TYPE_GMS)) {
+            handleGmsCheck(-1);
+        } else if (!canSkip()) {
+            launchGmsAccountSetup();
+        } else {
+            new Thread() {
+                @Override
+                public void run() {
+                    HttpURLConnection urlConnection = null;
+                    try {
+                        urlConnection = (HttpURLConnection) mGmsUri.openConnection();
+                        urlConnection.setInstanceFollowRedirects(false);
+                        urlConnection.setConnectTimeout(GMS_SOCKET_TIMEOUT_MS);
+                        urlConnection.setReadTimeout(GMS_SOCKET_TIMEOUT_MS);
+                        urlConnection.setUseCaches(false);
+                        urlConnection.getInputStream();
+                        handleGmsCheck(urlConnection.getResponseCode());
+                    } catch (IOException e) {
+                        Log.e(TAG, "Gms connection check exception "
+                                + e);
+                        handleGmsCheck(-1);
+                    } finally {
+                        if (urlConnection != null) {
+                            urlConnection.disconnect();
+                        }
+                    }
+                }
+            }.start();
+        }
+    }
+
+    private void handleGmsCheck(final int responseCode) {
+        if (getCallbacks().isCurrentPage(GmsAccountPage.this)) {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    // If FRP is enabled, we have to launch GMS
+                    if (responseCode != 204 && canSkip()) {
+                        if (mRestoreComplete || mBackupEnabled) {
+                            getCallbacks().onNextPage();
+                        } else {
+                            launchGmsRestorePage();
+                        }
+                    } else {
+                        launchGmsAccountSetup();
+                    }
+                }
+            });
+        }
     }
 
     private void launchGmsAccountSetup() {
