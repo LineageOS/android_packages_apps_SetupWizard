e6eb20cb0b9391310968f6c79f4610e3f45b3e02
Ethan Chen <intervigil@gmail.com>
2015-11-23 14:18:29 -0800

    SetupWizard: Set data subscription if not already set
    * On multi-SIM devices with only 1 SIM inserted, the SIM data
      subscription page is not shown.
    * Default the data subscription to the first subscription found if there
      is not one already set.
    
    Change-Id: I00d6f191dfd6af2bef24d12c1fd1790507e69f83

--- src/com/cyanogenmod/setupwizard/setup/MobileDataPage.java
+++ src/com/cyanogenmod/setupwizard/setup/MobileDataPage.java
@@ -24,6 +24,7 @@ import android.os.Handler;
 import android.telephony.PhoneStateListener;
 import android.telephony.ServiceState;
 import android.telephony.SignalStrength;
+import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
@@ -40,6 +41,8 @@ import com.cyanogenmod.setupwizard.SetupWizardApp;
 import com.cyanogenmod.setupwizard.cmstats.SetupStats;
 import com.cyanogenmod.setupwizard.ui.SetupPageFragment;
 import com.cyanogenmod.setupwizard.util.SetupWizardUtils;
+
+import java.util.List;
 
 public class MobileDataPage extends SetupPage {
 
@@ -161,6 +164,9 @@ public class MobileDataPage extends SetupPage {
             updateSignalStrength();
             if (SetupWizardUtils.isRadioReady(mContext, null)) {
                 hideWaitForRadio();
+                if (SetupWizardUtils.isMultiSimDevice(getActivity())) {
+                    setupDataSubscription();
+                }
             } else if (mTitleView != null) {
                 mTitleView.setText(R.string.loading);
                 mHandler.postDelayed(mRadioReadyRunnable, SetupWizardApp.RADIO_READY_TIMEOUT);
@@ -261,5 +267,18 @@ public class MobileDataPage extends SetupPage {
             return retVal;
         }
 
+        private void setupDataSubscription() {
+            // Set the first subscription we can find as default data subscription
+            // if there is not one already set
+            SubscriptionManager sm = SubscriptionManager.from(getActivity());
+            int dataSubId = SubscriptionManager.getDefaultDataSubId();
+            if (dataSubId == SubscriptionManager.INVALID_SUBSCRIPTION_ID) {
+                List<SubscriptionInfo> subInfoRecords = sm.getActiveSubscriptionInfoList();
+                // If there are no active subscriptions here, something is wrong
+                if (subInfoRecords != null && subInfoRecords.size() > 0) {
+                    sm.setDefaultDataSubId(subInfoRecords.get(0).getSubscriptionId());
+                }
+            }
+        }
     }
 }
