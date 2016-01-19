d59304c730d9ff61da8857ad18cf1e0646b363ce
Danesh M <daneshm90@gmail.com>
2016-01-18 18:39:02 -0800

    SetupWizard : Switch to mcc/mnc locale
    Also ensure we only send stats event once and
    only if the user actually changed the locale.
    
    CYNGNOS-1521
    CYNGNOS-1326
    
    Change-Id: I7b0451137e6bf80bf71350da28fe391fc8c52ce8

--- src/com/cyanogenmod/setupwizard/setup/CMSetupWizardData.java
+++ src/com/cyanogenmod/setupwizard/setup/CMSetupWizardData.java
@@ -89,6 +89,7 @@ public class CMSetupWizardData extends AbstractSetupData {
             showHideDataSimPage();
             showHideSimMissingPage();
             showHideMobileDataPage();
+            updateWelcomePage();
         } else if (intent.getAction()
                 .equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
             showHideMobileDataPage();
@@ -157,6 +158,13 @@ public class CMSetupWizardData extends AbstractSetupData {
         DateTimePage dateTimePage = (DateTimePage) getPage(DateTimePage.TAG);
         if (dateTimePage != null) {
             dateTimePage.setHidden(mTimeZoneSet & mTimeSet);
+        }
+    }
+
+    private void updateWelcomePage() {
+        WelcomePage welcomePage = (WelcomePage) getPage(WelcomePage.TAG);
+        if (welcomePage != null) {
+            welcomePage.simChanged();
         }
     }
 
