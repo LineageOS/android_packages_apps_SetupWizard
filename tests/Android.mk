d40d8b83c3fef6f0a89a87ef14ca30c6de2ed2f0
cretin45 <cretin45@gmail.com>
2016-02-01 13:53:56 -0800

    SetupWizard: Fix test app building in 13
    Change-Id: I53d5317070950d75637182c9284a7a38ec99c222

--- tests/Android.mk
+++ tests/Android.mk
@@ -1,17 +1,14 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-# We only want this apk build for tests.
-LOCAL_MODULE_TAGS := tests
+LOCAL_MODULE_TAGS := optional
 
-LOCAL_JAVA_LIBRARIES := android.test.runner
+LOCAL_PRIVILEGED_MODULE := true
 
 # Include all test java files.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := CyanogenSetupWizardTests
 LOCAL_CERTIFICATE := platform
-
-LOCAL_INSTRUMENTATION_FOR := CyanogenSetupWizard
 
 include $(BUILD_PACKAGE)
