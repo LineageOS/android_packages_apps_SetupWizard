1bfeb70dad077aa627659fe57e4c5ff7970c8cfb
Ed Mancebo <emancebo@cyngn.com>
2016-02-17 13:52:52 -0800

    Disable immersive mode confirmation for custom lockscreen package
    Change-Id: Iff92874727dbb0751aea1a48219acc5da6b64db7
    issue-id: YAM-134

--- src/com/cyanogenmod/setupwizard/setup/CyanogenSettingsPage.java
+++ src/com/cyanogenmod/setupwizard/setup/CyanogenSettingsPage.java
@@ -43,6 +43,7 @@ import android.util.Log;
 import android.view.IWindowManager;
 import android.view.View;
 import android.view.WindowManagerGlobal;
+import android.view.WindowManagerPolicyControl;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -190,11 +191,20 @@ public class CyanogenSettingsPage extends SetupPage {
                     Log.i(TAG, "Applying default lockscreen");
                     LockPatternUtils util = new LockPatternUtils(mContext);
                     util.setThirdPartyKeyguard(cn);
+                    disableImmersivePromptForLockscreen(cn);
                 } catch (PackageManager.NameNotFoundException | SecurityException e) {
                     Log.w(TAG, "Error setting default lockscreen: " + cn, e);
                 }
             }
         }
+    }
+
+    private void disableImmersivePromptForLockscreen(ComponentName cn) {
+        // prevents the immersive screen hint for the lockscreen
+        WindowManagerPolicyControl policy = new WindowManagerPolicyControl();
+        policy.reloadFromSetting(mContext);
+        policy.addToPreconfirmWhiteList(cn.getPackageName());
+        policy.saveToSettings(mContext);
     }
 
     private static boolean hideKeyDisabler(Context ctx) {
