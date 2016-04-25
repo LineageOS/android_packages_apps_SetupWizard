0de59b0e3739d651ce191d33696b97964b619eed
cretin45 <cretin45@gmail.com>
2016-04-25 11:00:04 -0700

    SetupWizard: Part 2: Implement new OOBE using custom wizard manager
    Change-Id: Ie6beb6395c5a4936384e78d57c95262e302220f9

---
+++ exit_wizard.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+adb root
+wait ${!}
+adb shell pm enable com.cyanogenmod.setupwizard/com.cyanogenmod.setupwizard.SetupWizardExitActivity || true
+wait ${!}
+adb shell pm enable com.google.android.setupwizard/com.google.android.setupwizard.SetupWizardExitActivity || true
+wait ${!}
+sleep 1
+adb shell am start com.cyanogenmod.setupwizard/com.cyanogenmod.setupwizard.SetupWizardExitActivity || true
+wait ${!}
+sleep 1
+adb shell am start com.google.android.setupwizard/com.google.android.setupwizard.SetupWizardExitActivity
