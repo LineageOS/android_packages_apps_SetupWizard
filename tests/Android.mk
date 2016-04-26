2e75c4ee0479d723e429ac29328ae884ecc51dd0
cretin45 <cretin45@gmail.com>
2016-04-26 10:52:22 -0700

    SetupWizard: Fix test app building
    Ticket: CYNGNOS-2431
    
    Change-Id: I75249f0bbb6e59f47e4a30608bb63b42c93c6e37

--- tests/Android.mk
+++ tests/Android.mk
@@ -5,6 +5,9 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_PRIVILEGED_MODULE := true
 
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    org.cyanogenmod.platform.sdk
+
 # Include all test java files.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
