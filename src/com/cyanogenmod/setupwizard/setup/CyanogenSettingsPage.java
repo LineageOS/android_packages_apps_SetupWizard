334c00f437a063dbc28417ef5722851755a5f7e7
cretin45 <cretin45@gmail.com>
2016-05-15 10:52:09 -0700

    SetupWizard: Use proper CMSetting for soft keys option
    Issue-id: CYNGNOS-2842
    
    Change-Id: I5ab75d9d90d79b66fa9b10f011272fc0090a78dd

--- src/com/cyanogenmod/setupwizard/setup/CyanogenSettingsPage.java
+++ src/com/cyanogenmod/setupwizard/setup/CyanogenSettingsPage.java
@@ -91,8 +91,8 @@ public class CyanogenSettingsPage extends SetupPage {
     private static void writeDisableNavkeysOption(Context context, boolean enabled) {
         final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 
-        CMSettings.Secure.putInt(context.getContentResolver(),
-                CMSettings.Secure.DEV_FORCE_SHOW_NAVBAR, enabled ? 1 : 0);
+        CMSettings.Global.putInt(context.getContentResolver(),
+                CMSettings.Global.DEV_FORCE_SHOW_NAVBAR, enabled ? 1 : 0);
         CMHardwareManager hardware = CMHardwareManager.getInstance(context);
         hardware.set(CMHardwareManager.FEATURE_KEY_DISABLE, enabled);
 
