143552b394bea67286e75f67162115c145157c5c
cretin45 <cretin45@gmail.com>
2016-05-23 14:17:08 -0700

    SetupWizard: Ensure language picker is enabled onResume
    Issue-id: FEIJ-559
    
    Change-Id: Iafe0467fc4130074ce86fe5f28b215d4774dc50e
    (cherry picked from commit 4981bc9888c5711bfebfe24e76dedd1422c2880a)

--- src/com/cyanogenmod/setupwizard/setup/WelcomePage.java
+++ src/com/cyanogenmod/setupwizard/setup/WelcomePage.java
@@ -356,6 +356,9 @@ public class WelcomePage extends SetupPage {
         public void onResume() {
             super.onResume();
             mPaused = false;
+            if (mLanguagePicker != null) {
+                mLanguagePicker.setEnabled(true);
+            }
             if (mPendingLocaleUpdate) {
                 mPendingLocaleUpdate = false;
                 fetchAndUpdateSimLocale();
