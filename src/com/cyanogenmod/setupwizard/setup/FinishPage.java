4f9028ae5e53a8d6ff5e87e52d9c63035b65d080
Roman Birg <roman@cyngn.com>
2016-06-14 15:49:39 -0700

    SetupWizard: toggle BT during setup
    We should toggle bluetooth during setup wizard so that Android has a
    chance to create its databases and let it initialize without the user
    expecting any action from the hardware. This way when the setup wizard
    is finished, the device is ready and initialized.
    
    Ticket: CYNGNOS-2866
    
    Change-Id: Ie981daa11f94fd698658085d2bcf24b8d8ba7e6a
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/cyanogenmod/setupwizard/setup/FinishPage.java
+++ src/com/cyanogenmod/setupwizard/setup/FinishPage.java
@@ -19,8 +19,14 @@ package com.cyanogenmod.setupwizard.setup;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -33,14 +39,54 @@ public class FinishPage extends SetupPage {
     public static final String TAG = "FinishPage";
 
     private FinishFragment mFinishFragment;
+    private boolean mBluetoothSetupFinished;
+    private boolean mUserCalledFinish;
+
+    private BroadcastReceiver mBluetoothStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,
+                    BluetoothAdapter.STATE_OFF);
+            if (state == BluetoothAdapter.STATE_ON) {
+                final BluetoothManager bt = mContext.getSystemService(BluetoothManager.class);
+                if (bt != null && bt.getAdapter() != null) {
+                    bt.getAdapter().disable();
+                    return;
+                }
+            }
+            mBluetoothSetupFinished = state == BluetoothAdapter.STATE_OFF;
+            if (mFinishFragment != null && mFinishFragment.enabledBlueooth()
+                    && mBluetoothSetupFinished && mUserCalledFinish) {
+                getCallbacks().onFinish();
+            }
+        }
+    };
 
     public FinishPage(Context context, SetupDataCallbacks callbacks) {
         super(context, callbacks);
+        final BluetoothManager bt = context.getSystemService(BluetoothManager.class);
+        if (bt != null && bt.getAdapter() != null && !bt.getAdapter().isEnabled()) {
+            mBluetoothSetupFinished = false;
+            context.registerReceiver(mBluetoothStateReceiver,
+                    new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
+        } else {
+            mBluetoothSetupFinished = true;
+            mBluetoothStateReceiver = null;
+        }
+    }
+
+    @Override
+    public void onFinishSetup() {
+        super.onFinishSetup();
+        if (mBluetoothStateReceiver != null) {
+            mContext.unregisterReceiver(mBluetoothStateReceiver);
+            mBluetoothStateReceiver = null;
+        }
     }
 
     @Override
     public Fragment getFragment(FragmentManager fragmentManager, int action) {
-        mFinishFragment = (FinishFragment)fragmentManager.findFragmentByTag(getKey());
+        mFinishFragment = (FinishFragment) fragmentManager.findFragmentByTag(getKey());
         if (mFinishFragment == null) {
             Bundle args = new Bundle();
             args.putString(Page.KEY_PAGE_ARGUMENT, getKey());
@@ -63,7 +109,18 @@ public class FinishPage extends SetupPage {
 
     @Override
     public boolean doNextAction() {
-        getCallbacks().onFinish();
+        if (mUserCalledFinish) {
+            return true;
+        }
+        mUserCalledFinish = true;
+        if (mBluetoothStateReceiver == null
+                || mFinishFragment == null || !mFinishFragment.enabledBlueooth()
+                || mBluetoothSetupFinished) {
+            // if we don't care about BT setup finish
+            // if fragment isn't up finish
+            // if BT setup finished before user hit next, finish
+            getCallbacks().onFinish();
+        }
         return true;
     }
 
@@ -74,6 +131,8 @@ public class FinishPage extends SetupPage {
 
     public static class FinishFragment extends SetupPageFragment {
 
+        private boolean mBluetoothEnabled;
+
         @Override
         protected void initializePage() {
             final Activity activity = getActivity();
@@ -83,6 +142,18 @@ public class FinishPage extends SetupPage {
                 mRootView.findViewById(R.id.mod_welcome).setVisibility(View.VISIBLE);
                 mRootView.findViewById(R.id.mod_desc).setVisibility(View.VISIBLE);
             }
+
+            // turn bluetooth on and off to let it initialize its databases
+            // and other things that take a _long_ time the first time around
+            // this way we are in a state ready to go (and beam!) on the first boot.
+            final BluetoothManager bt = getContext().getSystemService(BluetoothManager.class);
+            if (bt != null && bt.getAdapter() != null && !bt.getAdapter().isEnabled()) {
+                mBluetoothEnabled = bt.getAdapter().enable();
+            }
+        }
+
+        public boolean enabledBlueooth() {
+            return mBluetoothEnabled;
         }
 
         @Override
