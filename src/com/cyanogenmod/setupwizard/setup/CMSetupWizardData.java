a34de177b57a1046e06e52975c7a4ac6c7e19da6
Ricardo Cerqueira <ricardo@cyngn.com>
2016-08-10 12:33:05 +0100

    If the device has FRP, prompt the user to set up a lockscreen
    FRP is kind of pointless if the device is freely accessible. Suggest
    setting up a secure lockscreen during setup.
    
    Ref CYNGNOS-3252
    
    Change-Id: Ia807142ab08f388d533b5ee45d1fcbe81071f6d1

--- src/com/cyanogenmod/setupwizard/setup/CMSetupWizardData.java
+++ src/com/cyanogenmod/setupwizard/setup/CMSetupWizardData.java
@@ -74,6 +74,8 @@ public class CMSetupWizardData extends AbstractSetupData {
         }
         if (SetupWizardUtils.hasFingerprint(mContext) && SetupWizardUtils.isOwner()) {
             pages.add(new FingerprintSetupPage(mContext, this));
+        } else if (SetupWizardUtils.frpEnabled(mContext)) {
+            pages.add(new ScreenLockSetupPage(mContext, this));
         }
         pages.add(new CyanogenSettingsPage(mContext, this));
         pages.add(new OtherSettingsPage(mContext, this));
