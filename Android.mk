771554c4781ba207920e3fa9cd03e03cabb1b012
Abhisek Devkota <ciwrl@lineageos.org>
2018-01-29 12:41:42 -0800

    SUW: Update for LineageOS platform & sdk
    Change-Id: Idb765ea4808b4ba3c01414974e40ccf735f77f9d

--- Android.mk
+++ Android.mk
@@ -16,8 +16,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4 \
     android-support-v13 \
     libphonenumber \
-    org.cyanogenmod.platform.internal \
-    org.cyanogenmod.platform.sdk
+    org.lineageos.platform.internal \
+    org.lineageos.platform.sdk
 
 LOCAL_JAVA_LIBRARIES := \
     telephony-common
