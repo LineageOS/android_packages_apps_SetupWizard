af79d56bbd299146d88ee67a997efea786246220
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-22 08:51:25 -0800

    SuW: Explicitly include java support packages
    Change-Id: Ie80e18abb3b8d7b30734fbe6727c2978dbc30543

--- Android.mk
+++ Android.mk
@@ -15,6 +15,10 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4 \
     android-support-v13 \
+    android-support-v7-appcompat \
+    android-support-v7-preference \
+    android-support-v7-recyclerview \
+    android-support-v14-preference \
     libphonenumber \
     org.lineageos.platform.internal \
     org.lineageos.platform.sdk
