297b16f4bc2f27f38e7949f7e81d85bc83575e2f
Chirayu Desai <chirayudesai1@gmail.com>
2018-12-14 21:29:38 +0530

    tests: Add privapp permissions whitelist
    Change-Id: I080784198bdc726e49ed8d2c3fd9874220548b0f

--- tests/Android.mk
+++ tests/Android.mk
@@ -16,4 +16,14 @@ LOCAL_CERTIFICATE := platform
 
 LOCAL_PRIVATE_PLATFORM_APIS := true
 
+LOCAL_REQUIRED_MODULES := privapp_whitelist_org.lineageos.setupwizard-tests.xml
+
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_org.lineageos.setupwizard-tests.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
