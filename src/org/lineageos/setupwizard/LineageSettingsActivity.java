0ebdabef3ca984239adbf0209505cb3268fe2b3c
Chirayu Desai <chirayudesai1@gmail.com>
2019-09-13 01:12:59 +0530

    Update for Q setup library changes
    setupwizardlib,setupnavigationbar -> setupcompat,setupdesign
    
    Change-Id: I8bf75028984dbf9fc2341990e93bd15ca0ad2af8

--- src/org/lineageos/setupwizard/LineageSettingsActivity.java
+++ src/org/lineageos/setupwizard/LineageSettingsActivity.java
@@ -41,7 +41,7 @@ import android.view.WindowManagerGlobal;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
-import com.android.setupwizardlib.util.WizardManagerHelper;
+import com.google.android.setupcompat.util.WizardManagerHelper;
 
 import org.lineageos.setupwizard.R;
 
