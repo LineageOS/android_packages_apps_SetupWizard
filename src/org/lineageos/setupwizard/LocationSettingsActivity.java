9f3760eed017d405eb2eef81df337a3fe9b8c6c4
Bruno Martins <bgcngm@gmail.com>
2020-04-18 15:58:41 +0100

    LocationSettingsActivity: Account for deprecated location modes
    LOCATION_MODE_HIGH_ACCURACY, LOCATION_MODE_SENSORS_ONLY and
    LOCATION_MODE_BATTERY_SAVING are deprecated.
    
    Change-Id: Ibe1930902f6bfff99eb2c2d852ab600620e25f08

--- src/org/lineageos/setupwizard/LocationSettingsActivity.java
+++ src/org/lineageos/setupwizard/LocationSettingsActivity.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- * Copyright (C) 2017-2018 The LineageOS Project
+ * Copyright (C) 2017-2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,10 +35,6 @@ public class LocationSettingsActivity extends BaseSetupWizardActivity {
             LocationSettingsActivity.class.getSimpleName().substring(0, 22);
 
     private View mLocationRow;
-    private View mBatteryRow;
-    private View mNetworkRow;
-    private CheckBox mNetwork;
-    private CheckBox mBattery;
     private CheckBox mLocationAccess;
 
     private ContentResolver mContentResolver;
@@ -55,21 +51,13 @@ public class LocationSettingsActivity extends BaseSetupWizardActivity {
     private View.OnClickListener mLocationClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            onToggleLocationAccess(!mLocationAccess.isChecked());
-        }
-    };
-
-    private View.OnClickListener mBatteryClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            onToggleBatterySaving(!mBattery.isChecked());
-        }
-    };
-
-    private View.OnClickListener mNetworkClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            onToggleNetwork(!mNetwork.isChecked());
+            if (!mLocationAccess.isChecked()) {
+                mLocationAccess.setChecked(true);
+                setLocationMode(Settings.Secure.LOCATION_MODE_ON);
+            } else {
+                mLocationAccess.setChecked(false);
+                setLocationMode(Settings.Secure.LOCATION_MODE_OFF);
+            }
         }
     };
 
@@ -84,19 +72,6 @@ public class LocationSettingsActivity extends BaseSetupWizardActivity {
         mLocationRow = findViewById(R.id.location);
         mLocationRow.setOnClickListener(mLocationClickListener);
         mLocationAccess = (CheckBox) findViewById(R.id.location_checkbox);
-        mBatteryRow = findViewById(R.id.battery_saving);
-        mBatteryRow.setOnClickListener(mBatteryClickListener);
-        mBattery = (CheckBox) findViewById(R.id.battery_saving_checkbox);
-        mNetworkRow = findViewById(R.id.network);
-        mNetworkRow.setOnClickListener(mNetworkClickListener);
-        mNetwork = (CheckBox) findViewById(R.id.network_checkbox);
-        TextView networkSummary = (TextView) findViewById(R.id.network_summary);
-        final boolean hasTelephony = SetupWizardUtils.hasTelephony(this);
-        if (hasTelephony) {
-            networkSummary.setText(R.string.location_network_telephony);
-        } else {
-            networkSummary.setText(R.string.location_network);
-        }
     }
 
     @Override
@@ -144,78 +119,7 @@ public class LocationSettingsActivity extends BaseSetupWizardActivity {
             if (Log.isLoggable(TAG, Log.INFO)) {
                 Log.i(TAG, "Location mode has been changed");
             }
-            updateLocationToggles(mode);
+            mLocationAccess.setChecked(mode == Settings.Secure.LOCATION_MODE_ON);
         }
     }
-
-    private void updateLocationToggles(int mode) {
-        switch (mode) {
-            case Settings.Secure.LOCATION_MODE_OFF:
-                mLocationAccess.setChecked(false);
-                mBattery.setChecked(false);
-                mBattery.setEnabled(false);
-                mBatteryRow.setEnabled(false);
-                mNetwork.setChecked(false);
-                mNetwork.setEnabled(false);
-                mNetworkRow.setEnabled(false);
-                break;
-            case Settings.Secure.LOCATION_MODE_SENSORS_ONLY:
-                mLocationAccess.setChecked(true);
-                mBattery.setChecked(false);
-                mBattery.setEnabled(true);
-                mBatteryRow.setEnabled(true);
-                mNetwork.setChecked(false);
-                mNetwork.setEnabled(true);
-                mNetworkRow.setEnabled(true);
-                break;
-            case Settings.Secure.LOCATION_MODE_BATTERY_SAVING:
-                mLocationAccess.setChecked(true);
-                mBattery.setChecked(true);
-                mNetwork.setChecked(false);
-                mNetwork.setEnabled(false);
-                mNetworkRow.setEnabled(false);
-                break;
-            case Settings.Secure.LOCATION_MODE_HIGH_ACCURACY:
-                mLocationAccess.setChecked(true);
-                mNetwork.setChecked(true);
-                mBattery.setChecked(false);
-                mBattery.setEnabled(false);
-                mBatteryRow.setEnabled(false);
-                break;
-            default:
-                mLocationAccess.setChecked(false);
-                mBattery.setChecked(false);
-                mBattery.setEnabled(false);
-                mBatteryRow.setEnabled(false);
-                mNetwork.setChecked(false);
-                mNetwork.setEnabled(false);
-                mNetworkRow.setEnabled(false);
-                break;
-        }
-    }
-
-    private void onToggleLocationAccess(boolean checked) {
-        if (checked) {
-            setLocationMode(Settings.Secure.LOCATION_MODE_SENSORS_ONLY);
-        } else {
-            setLocationMode(Settings.Secure.LOCATION_MODE_OFF);
-        }
-    }
-
-    private void onToggleBatterySaving(boolean checked) {
-        if (checked) {
-            setLocationMode(Settings.Secure.LOCATION_MODE_BATTERY_SAVING);
-        } else {
-            setLocationMode(Settings.Secure.LOCATION_MODE_SENSORS_ONLY);
-        }
-    }
-
-    private void onToggleNetwork(boolean checked) {
-        if (checked) {
-            setLocationMode(Settings.Secure.LOCATION_MODE_HIGH_ACCURACY);
-        } else {
-            setLocationMode(Settings.Secure.LOCATION_MODE_SENSORS_ONLY);
-        }
-    }
-
 }
