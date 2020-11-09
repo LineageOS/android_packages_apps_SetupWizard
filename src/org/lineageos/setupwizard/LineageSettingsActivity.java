1229f410b7bfd7605b4f164ff978abc0afabd931
Christian Oder <myself5@carbonrom.org>
2020-11-09 18:12:08 +0100

    LSW: Fix backtrack to LineageSettings fragment
    The current handling results in the followup activity to ignore it for
    backtracks which means when going from Location -> LineageSettings -> Fingerprint
    the move backwards from Fingerprint leads up in Location and not the expected
    LineageSettings.
    
    The custom handling is necessary to process the result in the Google SUW,
    so it has been fixed to properly work now.
    
    Change-Id: Ifae5dc8fda07b61bd0271da1d1589059ab40cd39

--- src/org/lineageos/setupwizard/LineageSettingsActivity.java
+++ src/org/lineageos/setupwizard/LineageSettingsActivity.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- * Copyright (C) 2017-2019 The LineageOS Project
+ * Copyright (C) 2017-2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -134,14 +134,9 @@ public class LineageSettingsActivity extends BaseSetupWizardActivity {
     }
 
     @Override
-    public void onNavigateBack() {
-        onBackPressed();
-    }
-
-    @Override
-    public void onNavigateNext() {
+    protected void onNextPressed() {
         Intent intent = WizardManagerHelper.getNextIntent(getIntent(), Activity.RESULT_OK);
-        startActivityForResult(intent, 1);
+        nextAction(NEXT_REQUEST, intent);
     }
 
     @Override
