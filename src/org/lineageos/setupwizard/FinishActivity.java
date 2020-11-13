CalyxOS/platform_packages_apps_SetupWizard

Hide activities when setup finishes, so they don't pop up at the end

src/org/calyxos/setupwizard/FinishActivity.java
@@ -17,25 +17,18 @@
 
 package org.calyxos.setupwizard;
 
-import static org.calyxos.setupwizard.SetupWizardApp.DISABLE_NAV_KEYS;
-import static org.calyxos.setupwizard.SetupWizardApp.KEY_BUTTON_BACKLIGHT;
-import static org.calyxos.setupwizard.SetupWizardApp.KEY_PRIVACY_GUARD;
-import static org.calyxos.setupwizard.SetupWizardApp.KEY_SEND_METRICS;
+import static org.calyxos.setupwizard.SetupWizardApp.ACTION_SETUP_COMPLETE;
 import static org.calyxos.setupwizard.SetupWizardApp.LOGV;
 
 import android.animation.Animator;
 import android.app.Activity;
 import android.app.WallpaperManager;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.UserHandle;
-import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.widget.ImageView;
@@ -43,6 +36,9 @@ import android.widget.ImageView;
 import com.android.setupwizardlib.util.WizardManagerHelper;
 
 import org.calyxos.setupwizard.util.EnableAccessibilityController;
+
+import static android.os.Binder.getCallingUserHandle;
+import static org.calyxos.setupwizard.Manifest.permission.FINISH_SETUP;
 
 public class FinishActivity extends BaseSetupWizardActivity {
 
@@ -102,6 +98,10 @@ public class FinishActivity extends BaseSetupWizardActivity {
     }
 
     private void startFinishSequence() {
+        Intent i = new Intent(ACTION_SETUP_COMPLETE);
+        i.setPackage(getPackageName());
+        sendBroadcastAsUser(i, getCallingUserHandle(), FINISH_SETUP);
+
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
         hideBackButton();
         hideNextButton();

LineageOS/android_packages_apps_SetupWizard

Change-Id: Ica6b5babf7df7a7d8033ecd7694e3f45144b363a

src/org/lineageos/setupwizard/FinishActivity.java
@@ -17,6 +17,7 @@
 
 package org.lineageos.setupwizard;
 
+import static org.lineageos.setupwizard.SetupWizardApp.ACTION_SETUP_COMPLETE;
 import static org.lineageos.setupwizard.SetupWizardApp.DISABLE_NAV_KEYS;
 import static org.lineageos.setupwizard.SetupWizardApp.KEY_BUTTON_BACKLIGHT;
 import static org.lineageos.setupwizard.SetupWizardApp.KEY_SEND_METRICS;
@@ -44,6 +45,9 @@ import com.google.android.setupcompat.util.WizardManagerHelper;
 import org.lineageos.setupwizard.util.EnableAccessibilityController;
 
 import lineageos.providers.LineageSettings;
+
+import static android.os.Binder.getCallingUserHandle;
+import static org.lineageos.setupwizard.Manifest.permission.FINISH_SETUP;
 
 public class FinishActivity extends BaseSetupWizardActivity {
 
@@ -100,6 +104,10 @@ public class FinishActivity extends BaseSetupWizardActivity {
     }
 
     private void startFinishSequence() {
+        Intent i = new Intent(ACTION_SETUP_COMPLETE);
+        i.setPackage(getPackageName());
+        sendBroadcastAsUser(i, getCallingUserHandle(), FINISH_SETUP);
+
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
         hideBackButton();
         hideNextButton();