21337726afd636f967b0082b239c7da1d64fedae
Oliver Scott <olivercscott@gmail.com>
2020-12-30 16:02:20 -0500

    Add gesture to skip setup wizard
    Change-Id: I91226946103dd7c8f9076ed8e026bceee498a458

--- src/org/lineageos/setupwizard/WelcomeActivity.java
+++ src/org/lineageos/setupwizard/WelcomeActivity.java
@@ -17,11 +17,16 @@
 
 package org.lineageos.setupwizard;
 
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
 import org.lineageos.setupwizard.util.EnableAccessibilityController;
+import org.lineageos.setupwizard.util.SetupWizardUtils;
+
+import java.util.concurrent.TimeUnit;
 
 public class WelcomeActivity extends BaseSetupWizardActivity {
 
@@ -29,6 +34,9 @@ public class WelcomeActivity extends BaseSetupWizardActivity {
 
     private View mRootView;
     private EnableAccessibilityController mEnableAccessibilityController;
+    private GestureDetector mGestureDetector;
+    private MotionEvent previousTapEvent;
+    private int consecutiveTaps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +47,45 @@ public class WelcomeActivity extends BaseSetupWizardActivity {
         setBackDrawable(null);
         mEnableAccessibilityController =
                 EnableAccessibilityController.getInstance(getApplicationContext());
+        mGestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                Rect viewRect = new Rect();
+                int[] leftTop = new int[2];
+                mRootView.getLocationOnScreen(leftTop);
+                viewRect.set(
+                        leftTop[0], leftTop[1], leftTop[0] + mRootView.getWidth(), leftTop[1]
+                        + mRootView.getHeight());
+                if (viewRect.contains((int) e.getX(), (int) e.getY())) {
+                    if (isConsecutiveTap(e)) {
+                        consecutiveTaps++;
+                    } else {
+                        consecutiveTaps = 1;
+                    }
+                    if (consecutiveTaps == 4) {
+                        SetupWizardUtils.finishSetupWizard(WelcomeActivity.this);
+                    }
+                } else {
+                    // Touch outside the target view. Reset counter.
+                    consecutiveTaps = 0;
+                }
+
+                if (previousTapEvent != null) {
+                    previousTapEvent.recycle();
+                }
+                previousTapEvent = MotionEvent.obtain(e);
+                return false;
+            }
+        });
         mRootView.setOnTouchListener((v, event) ->
                 mEnableAccessibilityController.onTouchEvent(event));
-
+        mRootView.setOnTouchListener((v, event) ->
+                mGestureDetector.onTouchEvent(event));
     }
 
     @Override
@@ -55,5 +99,18 @@ public class WelcomeActivity extends BaseSetupWizardActivity {
     @Override
     protected int getLayoutResId() {
         return R.layout.welcome_activity;
+    }
+
+    private boolean isConsecutiveTap(MotionEvent currentTapEvent) {
+        if (previousTapEvent == null) {
+            return false;
+        }
+
+        double deltaX = previousTapEvent.getX() - currentTapEvent.getX();
+        double deltaY = previousTapEvent.getY() - currentTapEvent.getY();
+        long deltaTime = currentTapEvent.getEventTime() - previousTapEvent.getEventTime();
+        return (deltaX * deltaX + deltaY * deltaY >=
+                (mRootView.getWidth() * mRootView.getWidth()) / 2
+                && deltaTime < TimeUnit.SECONDS.toMillis(1));
     }
 }
