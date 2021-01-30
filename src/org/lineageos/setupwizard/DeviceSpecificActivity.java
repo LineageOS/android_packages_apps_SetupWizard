6116647a3f340e26881572fa34d5db16f7a96679
Michael W <baddaemon87@gmail.com>
2021-01-30 20:39:30 +0100

    SetupWizard: Add device specific hook page
    * Add a new activity DeviceSpecificActivity which acts as a jumppad
      into an activity using "org.lineageos.settings.device.SUW_SETTINGS"
    * In order to use this, copy the lineage_wizard_script.xml from
      /res/raw into your device overlays and provide an activity that
      * at least shows a back and continue button
      * returns RESULT_OK and RESULT_CANCELLED for the
        buttons
      * does whatever you need to specifically have done in setup!
    
    Change-Id: I4956c8f5f1ad13b9adf2e5604fd4a728e261336b

---
+++ src/org/lineageos/setupwizard/DeviceSpecificActivity.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package org.lineageos.setupwizard;
+
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.setupcompat.util.WizardManagerHelper;
+import org.lineageos.setupwizard.util.SetupWizardUtils;
+
+
+public class DeviceSpecificActivity extends WrapperSubBaseActivity {
+
+    private static final String ACTION_SETUP_DEVICE = "org.lineageos.settings.device.SUW_SETTINGS";
+
+    // random number that is in a range that is currently unused
+    private static final int REQUEST_CODE_SETUP_DEVICE = 90000;
+
+    @Override
+    protected void onStartSubactivity() {
+        Intent intent = new Intent(ACTION_SETUP_DEVICE);
+        startSubactivity(intent, REQUEST_CODE_SETUP_DEVICE);
+    }
+}
