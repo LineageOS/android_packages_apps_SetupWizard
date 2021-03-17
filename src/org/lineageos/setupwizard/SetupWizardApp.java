34bb71926207393e5173f8583af743df67c27a4f
Michael W <baddaemon87@gmail.com>
2021-03-17 22:47:01 +0100

    SetupWizard: Disable mobile data at least once
    * Right now, the user has to set the toggle on and off to actually get
      the setting set to "off", since the underlying default value is "on"
    * Make sure to send an "off" initially right at the beginning of SuW
    
    Change-Id: Iec2fd05ea341351f3e6d4b195a1a385f9d88d4f7

--- src/org/lineageos/setupwizard/SetupWizardApp.java
+++ src/org/lineageos/setupwizard/SetupWizardApp.java
@@ -94,6 +94,7 @@ public class SetupWizardApp extends Application {
         NetworkMonitor.initInstance(this);
         PhoneMonitor.initInstance(this);
         SetupWizardUtils.disableComponentsForMissingFeatures(this);
+        SetupWizardUtils.setMobileDataEnabled(MobileDataActivity.this, false);
         mHandler.postDelayed(mRadioTimeoutRunnable, SetupWizardApp.RADIO_READY_TIMEOUT);
     }
 
