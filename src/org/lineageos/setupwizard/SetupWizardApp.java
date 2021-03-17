25f12d0a531dea662386f599ca82c78508f1e07f
Michael W <baddaemon87@gmail.com>
2021-03-17 22:47:01 +0100

    SetupWizard: Disable mobile data at least once
    * Right now, the user has to set the toggle on and off to actually get
      the setting set to "off", since the underlying default value is "on"
    * Make sure to send an "off" initially right at the beginning of SuW
    
    Change-Id: Iec2fd05ea341351f3e6d4b195a1a385f9d88d4f7

--- src/org/lineageos/setupwizard/SetupWizardApp.java
+++ src/org/lineageos/setupwizard/SetupWizardApp.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2013 The CyanogenMod Project
- * Copyright (C) 2017-2020 The LineageOS Project
+ * Copyright (C) 2017-2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -94,6 +94,7 @@ public class SetupWizardApp extends Application {
         NetworkMonitor.initInstance(this);
         PhoneMonitor.initInstance(this);
         SetupWizardUtils.disableComponentsForMissingFeatures(this);
+        SetupWizardUtils.setMobileDataEnabled(this, false);
         mHandler.postDelayed(mRadioTimeoutRunnable, SetupWizardApp.RADIO_READY_TIMEOUT);
     }
 
