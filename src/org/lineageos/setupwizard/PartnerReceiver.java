06a0925860d194d0737558a091a84655154f03c6
Michael Bestas <mkbestas@lineageos.org>
2021-08-25 19:13:30 +0300

    The big code style cleanup
    * Format using AOSP code style in Android Studio,
      excluding `Rearrange entries` option.
    * Rename static variables to use proper naming scheme
    * Remove empty lines
    
    Change-Id: I0d193e35f071bc288050e587b1693844d68865e1

--- src/org/lineageos/setupwizard/PartnerReceiver.java
+++ src/org/lineageos/setupwizard/PartnerReceiver.java
@@ -1,5 +1,3 @@
-package org.lineageos.setupwizard;
-
 /*
  * Copyright (C) 2016 The CyanogenMod Project
  *
@@ -15,6 +13,8 @@ package org.lineageos.setupwizard;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.lineageos.setupwizard;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
