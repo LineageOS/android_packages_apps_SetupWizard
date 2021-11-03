2a4b5dbbbf669c122bc4784dcd92e5a632fe68e5
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-11-03 10:27:12 +0000

    SUW: layout for TV
    * Hide emergency call and accessibility options.
    
    Change-Id: I2d56129331c0ebb9d4b9273308ec2a0fa163f4b6

--- src/org/lineageos/setupwizard/LocaleActivity.java
+++ src/org/lineageos/setupwizard/LocaleActivity.java
@@ -37,6 +37,8 @@ import android.widget.Toast;
 import com.android.internal.telephony.MccTable;
 import com.android.internal.telephony.TelephonyIntents;
 
+import com.google.android.setupcompat.util.SystemBarHelper;
+
 import org.lineageos.setupwizard.R;
 import org.lineageos.setupwizard.widget.LocalePicker;
 
@@ -78,6 +80,9 @@ public class LocaleActivity extends BaseSetupWizardActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (SetupWizardUtils.hasLeanback(this)) {
+            SystemBarHelper.setBackButtonVisible(getWindow(), true);
+        }
         setNextText(R.string.next);
         mLanguagePicker = (LocalePicker) findViewById(R.id.locale_list);
         loadLanguages();
