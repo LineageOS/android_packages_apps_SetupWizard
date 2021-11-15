6df6bb12e5c24cf1777d0ba2511248674fa04c84
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-11-15 18:01:38 +0000

    SUW: show the link to privacy policy
    Leave the link unclicable on large-notouch
    
    Change-Id: Icd1d193b74db963a12262deca0e6fbb028d4d504

--- src/org/lineageos/setupwizard/LineageSettingsActivity.java
+++ src/org/lineageos/setupwizard/LineageSettingsActivity.java
@@ -72,32 +72,9 @@ public class LineageSettingsActivity extends BaseSetupWizardActivity {
         super.onCreate(savedInstanceState);
         mSetupWizardApp = (SetupWizardApp) getApplication();
         setNextText(R.string.next);
-        String privacy_policy = getString(R.string.services_privacy_policy);
-        String policySummary = getString(R.string.services_explanation, privacy_policy);
-        SpannableString ss = new SpannableString(policySummary);
-        if (!getResources().getBoolean(R.bool.config_isLargeNoTouch)) {
-            ClickableSpan clickableSpan = new ClickableSpan() {
-                @Override
-                public void onClick(View textView) {
-                    // At this point of the setup, the device has already been unlocked (if frp
-                    // had been enabled), so there should be no issues regarding security
-                    final Intent intent = new Intent(Intent.ACTION_VIEW,
-                            Uri.parse(PRIVACY_POLICY_URI));
-                    try {
-                        startActivity(intent);
-                    } catch (Exception e) {
-                        Log.e(TAG, "Unable to start activity " + intent.toString(), e);
-                    }
-                }
-            };
-            int ppStartIndex = policySummary.indexOf(privacy_policy);
-            ss.setSpan(clickableSpan,
-                    ppStartIndex, ppStartIndex + privacy_policy.length(),
-                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        }
-        TextView privacyPolicy = (TextView) findViewById(R.id.privacy_policy);
-        privacyPolicy.setMovementMethod(LinkMovementMethod.getInstance());
-        privacyPolicy.setText(ss);
+
+        String policySummary = getString(R.string.services_find_privacy_policy, PRIVACY_POLICY_URI);
+        ((TextView) findViewById(R.id.find_privacy_policy)).setText(policySummary);
 
         View metricsRow = findViewById(R.id.metrics);
         metricsRow.setOnClickListener(mMetricsClickListener);
