f2a42b051d0f1aa0744ca2187e731f464a15c4f2
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-11-20 10:00:25 +0000

    SUW: use https for privacy policy link
    Change-Id: I6cbdb30f08c16233cf75af6bb407974411002dcf

--- src/org/lineageos/setupwizard/LineageSettingsActivity.java
+++ src/org/lineageos/setupwizard/LineageSettingsActivity.java
@@ -46,7 +46,7 @@ public class LineageSettingsActivity extends BaseSetupWizardActivity {
 
     public static final String TAG = LineageSettingsActivity.class.getSimpleName();
 
-    public static final String PRIVACY_POLICY_URI = "http://lineageos.org/legal";
+    public static final String   = "https://lineageos.org/legal";
 
     private SetupWizardApp mSetupWizardApp;
 
