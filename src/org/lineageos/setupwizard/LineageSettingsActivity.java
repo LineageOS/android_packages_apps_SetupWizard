3fbffd8ceeea04d3cac3ae776be3460e01bf3ef7
Timi Rautamäki <timi.rautamaki@gmail.com>
2022-01-14 21:55:13 +0000

    SUW: update TV theming for 12
    * setupdesign now uses a layout-land
    * Rearrange welcome activity
    * Add a padding of 56dp for each activity based on
      external/setupdesign/main/res/values/dimens.xml
      ´sud_glif_icon_margin_top´
    
    Change-Id: I77ec3d1c3d62a66aeb76233eccafbea96804e701

--- src/org/lineageos/setupwizard/LineageSettingsActivity.java
+++ src/org/lineageos/setupwizard/LineageSettingsActivity.java
@@ -77,8 +77,9 @@ public class LineageSettingsActivity extends BaseSetupWizardActivity {
         String privacyPolicy = getString(R.string.services_pp_explanation, os_name);
         String policySummary = getString(R.string.services_find_privacy_policy, PRIVACY_POLICY_URI);
 
-        ((TextView) findViewById(R.id.privacy_policy)).setText(privacyPolicy);
-        ((TextView) findViewById(R.id.find_privacy_policy)).setText(policySummary);
+        //((TextView) findViewById(R.id.privacy_policy)).setText(privacyPolicy);
+        //((TextView) findViewById(R.id.find_privacy_policy)).setText(policySummary);
+        getGlifLayout().setDescriptionText(privacyPolicy + "\n" + policySummary);
 
         View metricsRow = findViewById(R.id.metrics);
         metricsRow.setOnClickListener(mMetricsClickListener);
