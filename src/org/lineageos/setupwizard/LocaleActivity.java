65ab2e642494131c2393bd68eb9c2b6da2efaafa
Timi Rautamäki <timi.rautamaki@gmail.com>
2022-01-15 09:25:02 +0000

    SUW: improve navigation on D-pad devices
    * Set the initial focus on a usable view instead of a whole layout.
    * Set the next focus of the locale selector to next-button.
    
    Change-Id: I4a16127c24e87b52e38892882e44b3a8d24b779b

--- src/org/lineageos/setupwizard/LocaleActivity.java
+++ src/org/lineageos/setupwizard/LocaleActivity.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- * Copyright (C) 2017-2021 The LineageOS Project
+ *               2017-2022 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -81,6 +81,8 @@ public class LocaleActivity extends BaseSetupWizardActivity {
         SystemBarHelper.setBackButtonVisible(getWindow(), true);
         setNextText(R.string.next);
         mLanguagePicker = (LocalePicker) findViewById(R.id.locale_list);
+        mLanguagePicker.setNextRight(getNextButton().getId());
+        mLanguagePicker.requestFocus();
         loadLanguages();
     }
 
