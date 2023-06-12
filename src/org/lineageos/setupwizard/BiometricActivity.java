bfa9a5b0f0c796055ef093e56071259bbf13a7fb
Oliver Scott <olivercscott@gmail.com>
2023-06-12 16:56:47 -0400

    Launch Settings activities for lockscreen and biometrics setup
    Issue: calyxos#1420
    Change-Id: If6145a7f5a8bd0acc648a3b3ce4aa0d59a5157e0

--- src/org/lineageos/setupwizard/BiometricActivity.java
+++ src/org/lineageos/setupwizard/BiometricActivity.java
@@ -18,76 +18,17 @@
 package org.lineageos.setupwizard;
 
 import static org.lineageos.setupwizard.SetupWizardApp.ACTION_SETUP_BIOMETRIC;
-import static org.lineageos.setupwizard.SetupWizardApp.EXTRA_DETAILS;
-import static org.lineageos.setupwizard.SetupWizardApp.EXTRA_TITLE;
 import static org.lineageos.setupwizard.SetupWizardApp.REQUEST_CODE_SETUP_BIOMETRIC;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.widget.TextView;
 
-import com.google.android.setupcompat.util.WizardManagerHelper;
-import com.google.android.setupdesign.util.ThemeHelper;
-
-import org.lineageos.setupwizard.util.SetupWizardUtils;
-
-public class BiometricActivity extends SubBaseActivity {
+public class BiometricActivity extends WrapperSubBaseActivity {
 
     public static final String TAG = BiometricActivity.class.getSimpleName();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        final TextView setupBiometricSummary = (TextView) findViewById(
-                R.id.setup_biometric_summary);
-        final TextView setupAddBiometric = (TextView) findViewById(R.id.setup_add_biometric);
-        if (SetupWizardUtils.hasFace(this)) {
-            setupBiometricSummary.setText(getString(R.string.face_setup_summary));
-            setupAddBiometric.setText(R.string.face_setup_add_face);
-        } else {
-            setupBiometricSummary.setText(getString(R.string.fingerprint_setup_summary));
-            setupAddBiometric.setText(R.string.fingerprint_setup_add_fingerprint);
-        }
-    }
-
-    @Override
-    protected void onNextPressed() {
-        launchBiometricSetup();
-    }
-
-    @Override
     protected void onStartSubactivity() {
-        setNextAllowed(true);
-    }
-
-    @Override
-    protected int getLayoutResId() {
-        return R.layout.setup_biometric;
-    }
-
-    @Override
-    protected int getTitleResId() {
-        if (SetupWizardUtils.hasFace(this)) {
-            return R.string.face_setup_title;
-        }
-        return R.string.fingerprint_setup_title;
-    }
-
-    @Override
-    protected int getIconResId() {
-        if (SetupWizardUtils.hasFace(this)) {
-            return R.drawable.ic_face;
-        }
-        return R.drawable.ic_fingerprint;
-    }
-
-    private void launchBiometricSetup() {
         Intent intent = new Intent(ACTION_SETUP_BIOMETRIC);
-        intent.putExtra(EXTRA_TITLE,
-                getString(getTitleResId()));
-        intent.putExtra(EXTRA_DETAILS,
-                getString(R.string.settings_biometric_setup_details));
-        intent.putExtra(WizardManagerHelper.EXTRA_IS_SETUP_FLOW, true);
         startSubactivity(intent, REQUEST_CODE_SETUP_BIOMETRIC);
     }
 }
