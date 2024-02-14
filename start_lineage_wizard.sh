ec05005cfec103a4c8862e5bed186e10572f6e17
Michael Bestas <mkbestas@lineageos.org>
2024-02-14 20:35:43 +0200

    SetupWizard: Make it REUSE compliant
    * Ignored all the strings.xml since they are auto-synced
    
    Change-Id: I5d9e7aa5ae7b0079f792450f7e6dbf83216ebac4

--- start_lineage_wizard.sh
+++ start_lineage_wizard.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
 adb root
 wait ${!}
 adb shell pm enable org.lineageos.setupwizard || true
