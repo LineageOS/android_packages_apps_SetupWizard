e868ac4b32877a6f7f3ac44bf17cb229c1565cc9
Tommy Webb <tommy@calyxinstitute.org>
2024-02-21 18:06:55 +0000

    fixup! Only finish Setup Wizard Activity when fully done
    Change-Id: I6168437be449bb4c9f95dadcc24b1cf4cca8f177

--- exit_wizard.sh
+++ exit_wizard.sh
@@ -6,18 +6,18 @@
 adb root
 wait ${!}
 has_google_suw=$(adb shell pm list packages com.google.android.setupwizard)
-adb shell pm enable org.lineageos.setupwizard/.SetupWizardExitActivity || true
+adb shell pm enable org.lineageos.setupwizard/.FinishActivity || true
 if [[ ! -z "$has_google_suw" ]]
 then
     wait ${!}
-    adb shell pm enable com.google.android.setupwizard/.SetupWizardExitActivity || true
+    adb shell pm enable com.google.android.setupwizard/.FinishActivity || true
     wait ${!}
 fi
 sleep 1
-adb shell am start org.lineageos.setupwizard/.SetupWizardExitActivity || true
+adb shell am start org.lineageos.setupwizard/.FinishActivity || true
 if [[ ! -z "$has_google_suw" ]]
 then
     wait ${!}
     sleep 1
-    adb shell am start com.google.android.setupwizard/.SetupWizardExitActivity
+    adb shell am start com.google.android.setupwizard/.FinishActivity
 fi
