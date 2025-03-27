09cdf76e5ef79cd6612522e1c10445b9431687fa
Tommy Webb <tommy@calyxinstitute.org>
2025-03-27 17:36:03 +0000

    FinishActivity: Also hide Back button
    At the point of no return, also hide the Back button, not just Next.
    
    Change-Id: Ic7b2bf443306b49d7771eaf69e6fdede3fb41fa9

--- src/org/lineageos/setupwizard/FinishActivity.java
+++ src/org/lineageos/setupwizard/FinishActivity.java
@@ -86,7 +86,7 @@ public class FinishActivity extends BaseSetupWizardActivity {
         });
 
         if (sFinishState != FinishState.NONE) {
-            hideNextButton();
+            disableNavigation();
         }
 
         switch (sFinishState) {
@@ -103,6 +103,11 @@ public class FinishActivity extends BaseSetupWizardActivity {
                 Log.w(TAG, "Unexpected onCreate state " + sFinishState);
                 break;
         }
+    }
+
+    private void disableNavigation() {
+        hideNextButton();
+        SystemBarHelper.setBackButtonVisible(getWindow(), false);
     }
 
     private void disableActivityTransitions() {
@@ -169,7 +174,7 @@ public class FinishActivity extends BaseSetupWizardActivity {
     private void startFinishSequence() {
         sFinishState = FinishState.ANIMATING;
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
-        hideNextButton();
+        disableNavigation();
 
         // Begin outro animation.
         if (mRootView.isAttachedToWindow()) {
