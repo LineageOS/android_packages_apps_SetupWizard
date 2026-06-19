ae87a686a8d3b61c0bc4532589d704b956db6c9c
Saalim Quadri <danascape@gmail.com>
2026-06-19 20:27:59 +0530

    SUW: Convert PartnerReceiver to kt
    Change-Id: Ia39dd7030aba7844ec72367e96a6d83a8533ec58
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

---
+++ app/src/main/java/org/lineageos/setupwizard/receiver/PartnerReceiver.kt
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.setupwizard.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class PartnerReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        // Do nothing
+    }
+}
