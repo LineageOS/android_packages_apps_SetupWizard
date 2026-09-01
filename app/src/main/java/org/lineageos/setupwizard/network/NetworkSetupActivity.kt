f6b8cd64c08e7b367580be4dca3b7e3e76969d59
Saalim Quadri <danascape@gmail.com>
2026-09-01 20:23:36 +0530

    SUW: Use setup design footer bar for network setup
    Change-Id: I2dac04e7658bd4bbc7bb75cb32715494472b54a4
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- app/src/main/java/org/lineageos/setupwizard/network/NetworkSetupActivity.kt
+++ app/src/main/java/org/lineageos/setupwizard/network/NetworkSetupActivity.kt
@@ -7,6 +7,7 @@
 package org.lineageos.setupwizard.network
 
 import android.content.Intent
+import com.google.android.setupcompat.partnerconfig.PartnerConfigHelper
 import com.google.android.setupcompat.util.ResultCodes.RESULT_SKIP
 import org.lineageos.setupwizard.SetupWizardApp
 import org.lineageos.setupwizard.base.SubBaseActivity
@@ -29,8 +30,10 @@ class NetworkSetupActivity : SubBaseActivity() {
 
         val intent =
             Intent(ACTION_SETUP_NETWORK).apply {
-                putExtra(SetupWizardApp.EXTRA_PREFS_SHOW_BUTTON_BAR, true)
-                putExtra(SetupWizardApp.EXTRA_PREFS_SHOW_SKIP, true)
+                if (!PartnerConfigHelper.isGlifExpressiveEnabled(this)) {
+                    putExtra(SetupWizardApp.EXTRA_PREFS_SHOW_BUTTON_BAR, true)
+                    putExtra(SetupWizardApp.EXTRA_PREFS_SHOW_SKIP, true)
+                }
                 putExtra(SetupWizardApp.EXTRA_PREFS_SHOW_SKIP_TV, true)
                 putExtra(SetupWizardApp.EXTRA_PREFS_SET_BACK_TEXT, null as String?)
                 putExtra(SetupWizardApp.EXTRA_ENABLE_NEXT_ON_CONNECT, true)
